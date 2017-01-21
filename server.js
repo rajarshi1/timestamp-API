3a9947ca4be6d234e7339060998c3d6fd627062f
rajarshi1 <ghoshal.rajarshi@gmail.com>
2017-01-21 12:10:26 +0000

    add timestamp API

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
-var express = require('express')
-var app = express()
+'use strict';
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+const path = require('path');
+const moment = require('moment');
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
-})
+const express = require('express');
+const app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(path.resolve(__dirname, 'client')));
+app.get('/:timestamp', (req,res) => {
+  let time = moment(req.params.timestamp, 'MMMM DD, YYYY', true);
+  if (!time.isValid())
+    time = moment.unix(req.params.timestamp);
+  
+  if (!time.isValid()) {
+    res.json({
+      'humanReadable': null,
+      'unix': null
+    });
+  }
+  
+  res.json({
+    'humanReadable': time.format('MMMM DD, YYYY'),
+    'unix': time.format('X')
+  });
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`Server listening on port ${app.get('port')}`);
+});
